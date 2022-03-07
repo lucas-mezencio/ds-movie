2081c6bb891861d763220a857d0afe0fae93b304
Lucas Mezencio <lucasmezss@hotmail.com>
2022-03-07 17:07:15 -0300

    add moviestars component

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,17 @@
+import { ReactComponent as StarFull } from "assets/img/full-star.svg";
+import { ReactComponent as StarHalf } from "assets/img/half-star.svg";
+import { ReactComponent as StarEmpty } from "assets/img/empty-star.svg";
+
+function MovieStars() {
+  return (
+    <div className="dsmovie-stars-container">
+      <StarFull />
+      <StarFull />
+      <StarFull />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  );
+}
+
+export default MovieStars;
